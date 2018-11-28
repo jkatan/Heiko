69659511c2f263b43faf137e5493a00d78716811
matiheimann <mheimann@itba.edu.ar>
2018-11-28 17:37:37 -0300

    Merge util and basicutils

--- util.h
+++ util.h
@@ -5,5 +5,13 @@ void resize(int *vector_max_size, int vector_index, char ***vector_initializator
 int calculate_vector_length(int vector_index, char **vector_initializator);
 void free_vector(int *vector_index, int *vector_max_size, int *vector_size, char ***vector_initializator);
 int validate_matrix_column_size(int matrix_current_vec_size, int vector_size);
+void printsumarrays();
+void printsubarrays();
+void printmultiarrays();
+void printsummatrix();
+void printsubmatrix();
+void printmultimatrix();
+void printprintmatrix();
+void printprintarray();
 
 #endif
