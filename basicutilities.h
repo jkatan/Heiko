f9d80e73b4890ae51c07ff9024ae7319ccc780de
matiheimann <mheimann@itba.edu.ar>
2018-11-28 17:35:42 -0300

    Basic prints

---
+++ basicutilities.h
@@ -0,0 +1,13 @@
+#ifndef BASIC_UTILITIES
+#define BASIC_UTILITIES
+
+void printsumarrays();
+void printsubarrays();
+void printmultiarrays();
+void printsummatrix();
+void printsubmatrix();
+void printmultimatrix();
+void printprintmatrix();
+void printprintarray();
+
+#endif
